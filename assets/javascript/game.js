2ee35f408352ab1c9d56940cfae3f27d3d91e949
Rachael <rdougherty031@gmail.com>
2019-02-20 17:55:38 -0500

    Game over alerts

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -48,82 +48,83 @@ var phillyWords = [
     "wawa",
     "juneteenth"
 ];
-
+var wins = 0;
+var losses = 0;
+var guessesLeft=20;
+var correctLetters=0;
+var unguessedWord = [];
 var guessedLetters = [];
-console.log(phillyWords.length);
+
 
 document.onkeypress = function(event) {
 
     //Generates random word from phillyWords array
-        var unguessedWord = [];
         var wordToGuess = phillyWords[Math.floor(Math.random()*phillyWords.length)];
         console.log(wordToGuess);
 
+
     //creates new array unguessedWord and pushes _ for each letter of wordToGuess and " " for each space
-
-    //!!!!!!!*****REMOVE COMMAS ***********
-
         for (i=0; i<wordToGuess.length; i++) {
             if (wordToGuess.charAt(i) === " ") {
-                unguessedWord.push(" ");
+                unguessedWord.push("_");
             }
             else {
-                unguessedWord.push("_");
+                unguessedWord.push("-");
             }
             console.log(unguessedWord);
         }
 
-    //Puts unguessedWord in innerHTML of div#guessWord but hides visibility
-    document.getElementById("guessWord").innerHTML=unguessedWord;
-    
-    //prompt user to guess a letter
-    document.getElementById("currentMessage").innerHTML="Guess a letter.";
-    document.onkeypress = function(event) {
-        var guess = event.key.toLowerCase();
-        if (guessedLetters.includes(guess)) {
-            document.getElementById("currentMessage").innerHTML="You already guessed this letter. Guess again";
-        }
-        else {
-            guessedLetters.push(guess);
-            document.getElementById("lettersGuessed").innerHTML= guessedLetters;
-            console.log(guess);
-            console.log(guessedLetters);
-            
-            for (i=0; i<wordToGuess.length; i++) {
-                if (guess === wordToGuess.charAt(i)) {
-                    unguessedWord.splice(i,1,guess);
-                    console.log(unguessedWord);
-                    document.getElementById("guessWord").innerHTML=unguessedWord;
+    //Puts unguessedWord in innerHTML & replaces commas with spaces
+    document.getElementById("guessWord").innerHTML=unguessedWord.join(" ");
+
+        //prompt user to guess a letter
+        document.getElementById("currentMessage").innerHTML="Guess a letter.";
+        document.onkeypress = function(event) {
+            var guess = event.key.toLowerCase();
+            if (guessedLetters.includes(guess)) {
+                document.getElementById("currentMessage").innerHTML="You already guessed this letter. Guess again";
+            }
+            else {
+                guessesLeft--;
+                guessedLetters.push(guess);
+                document.getElementById("lettersGuessed").innerHTML= guessedLetters;
+                console.log(guess);
+                console.log(guessedLetters);
+                console.log(guessesLeft);
+                document.getElementById("guessesLeft").innerHTML=guessesLeft;
+
+                
+                for (i=0; i<wordToGuess.length; i++) {
+                    if (guess === wordToGuess.charAt(i)) {
+                        unguessedWord.splice(i,1,guess);
+                        correctLetters++;
+                        console.log(unguessedWord);
+                        console.log(correctLetters);
+                        document.getElementById("guessWord").innerHTML=unguessedWord.join(" ");
+                        document.getElementById("currentMessage").innerHTML="Good guess!";
+                        document.getElementById("correctLetters").innerHTML=correctLetters;
+                    }
+                    else {
+                        document.getElementById("currentMessage").innerHTML="Nice try. Guess again";
+                    }
+                //check if win or lose -- win broken
+                if (!unguessedWord.includes("-")) 
+                        {
+                            alert("you win!");
+                            wins++;
+                            document.getElementById("wins").innerHTML=wins;
+                            break;
+                        }
+                else if (guessesLeft === 0) {
+                    alert("you lose!");
+                    losses++;
+                    document.getElementById("wins").innerHTML=wins;
+                    break;
                 }
                 else {
-                    document.getElementById("currentMessage").innerHTML="Nice try. Guess again";
+                    continue;
+                }
                 }
             }
         }
     }
-
-
-//checks to see if letter already guessed, if not checks to see if in wordToGuess
-//if in wordToGuess, assigns the letter to the character index of unguessedWord & reveals to user.
-//if not in wordToGuess, tells user "Nice try. Guess again."
-
-
-
-    // for (i=0; i<guessedLetters.length; i++) {
-
-    //     if {
-    //         for (i=0; i<wordToGuess.length; i++) {
-    //             if (guess === wordToGuess.charAt(i)) {
-    //                 unguessedWord.replace(indexOf(i), guess);
-    //                 console.log(unguessedWord);
-    //             }
-    //             else {
-    //                 document.getElementById("guessAgain").innerHTML="Nice try. Guess again";
-    //             }
-    //         console.log(guessedLetters.push(guess));
-    //         document.getElementById("lettersGuessed").innerHTML=guess + " ";
-    //         }
-    //     }
-    // }
-
-}
